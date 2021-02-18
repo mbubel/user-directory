ca51b318a0a5149cf2311aa6db4151c601031e2d
Michael <michaelbubel.1989@gmail.com>
2021-02-17 19:13:45 -0800

    Employee data shows up

--- userdirectory/src/App.js
+++ userdirectory/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import EmployeeTable from "./components/EmployeeTable"
 import "./App.css";
 function App() {
   return (
     <div className="App">
       <Header />
+      <EmployeeTable/>
     </div>
   );
 }
---
+++ userdirectory/src/components/EmployeeTable.js
@@ -0,0 +1,41 @@
+import { Component } from "react";
+import EmployeeTableData from "./EmployeeTableData";
+import API from "../Utils/API";
+
+class EmployeeTable extends Component {
+  state = {
+    results: [],
+  };
+
+  componentDidMount() {
+    this.searchEmployees();
+  }
+  searchEmployees = () => {
+    API.search().then((res) =>
+      this.setState({ results: res.data.results })
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Email</th>
+              <th>Dob</th>
+            </tr>
+          </thead>
+          <tbody>
+            <EmployeeTableData employeeList={this.state.results} />
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default EmployeeTable;
