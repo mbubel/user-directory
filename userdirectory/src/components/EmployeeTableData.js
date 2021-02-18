5d3379b09f604c649f88c125a91e13f9a8324a04
Michael <michaelbubel.1989@gmail.com>
2021-02-17 20:14:07 -0800

    removing console logs

--- userdirectory/src/components/EmployeeTableData.js
+++ userdirectory/src/components/EmployeeTableData.js
@@ -1,22 +1,22 @@
 function EmployeeTableData(props) {
-    console.log(props);
-    return (
-      <>
-        {props.employeeList.map((result) => (
-          <tr>
-            <td>
-              <img src={result.picture.medium} alt={result.name.first} />
-            </td>
-            <td>
-              {result.name.first} {result.name.last}
-            </td>
-            <td>{result.phone}</td>
-            <td>{result.email}</td>
-            <td>{result.dob.date}</td>
-          </tr>
-        ))}
-      </>
-    );
-  }
-  
-  export default EmployeeTableData;
+  return (
+    <>
+      {" "}
+      {props.employeeList.map((result) => (
+        <tr>
+          <td>
+            <img src={result.picture.medium} alt={result.name.first} />
+          </td>
+          <td>
+            {result.name.first} {result.name.last}
+          </td>
+          <td>{result.phone}</td>
+          <td>{result.email}</td>
+          <td>{result.dob.date}</td>
+        </tr>
+      ))}
+    </>
+  );
+}
+
+export default EmployeeTableData;
