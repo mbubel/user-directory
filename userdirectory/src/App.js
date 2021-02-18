21c30f509de0e82fa3197bdc6fe3d93fe5fce2a2
Michael <michaelbubel.1989@gmail.com>
2021-02-17 19:25:29 -0800

    added a search bar to the top

--- userdirectory/src/App.js
+++ userdirectory/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./components/Header";
-import EmployeeTable from "./components/EmployeeTable"
+import EmployeeTable from "./components/EmployeeTable";
+import Navbar from "./components/Navbar";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css';
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <EmployeeTable/>
+       <Navbar />
+      <EmployeeTable />
     </div>
   );
 }
---
+++ userdirectory/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Navbar() {
+  return (
+    <nav>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+        Search
+      </button>{" "}
+      <input />
+    </nav>
+  );
+}
+export default Navbar;
