32e5b8bdcecf9a567e5c98e5c4fd7e345f2d8845
Michael <michaelbubel.1989@gmail.com>
2021-02-17 20:11:47 -0800

    removed unnecessary files

--- userdirectory/src/App.js
+++ userdirectory/src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 import EmployeeTable from "./components/EmployeeTable";
 import Navbar from "./components/Navbar";
-import "./App.css";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-       <Navbar />
+      <Navbar />
       <EmployeeTable />
     </div>
   );
