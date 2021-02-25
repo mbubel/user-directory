2c6c35ad4e6708de158f1d7615b3aa925ce9611a
Michael <michaelbubel.1989@gmail.com>
2021-02-25 14:13:01 -0800

    removed unnecessary folders

--- userdirectory/src/components/Header.js
+++ userdirectory/src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="header">
       <h1>Employee Directory</h1>
-      <p>Search for an employee using their first name, and list names alphabetically from A-Z.</p>
+      <p>Sort employee's by their first name, and then search for a specific employee!</p>
     </div>
   );
 }
